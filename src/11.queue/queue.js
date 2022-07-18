f3cd7d2a5dbdc0d1d9572de96b76aa324af8a500
khg <poohghg@naver.com>
2022-07-18 19:02:10 +0900

    2022-07-18 큐정리

--- src/11.queue/queue.js
+++ src/11.queue/queue.js
@@ -1,18 +1,20 @@
 // 배열로 스택을 만들기
-const stackInArray = () => {
-  const stack = [];
+const queueInArray = () => {
+  // 큐는 선입선출이다.
+  const q = [];
   //  js의 내장메서드인 push와 pop만 사용한다면 스택처럼 배열을 관리 할 수 있다.
-  stack.push(1);
-  stack.push(2);
-  stack.push(3);
-  console.log(stack.pop());
-  console.log(stack);
+  q.unshift(1);
+  q.unshift(2);
+  q.unshift(3);
+  console.log(q);
+  console.log(q.pop());
+  console.log(q);
   return undefined;
   // shift 와 unshift를 사용해서 배열의 index를 재정렬하는것은 좋지 않다.
 };
-// stackInArray();
-const stackInLinkedList = () => {
-  // 링크드리스로 이루어진 스택은 삽입과 삭제시 시간복잡도에서 상수시간을 가진다.
+// queueInArray();
+const queueInLinkedList = () => {
+  // 링크드리스로 이루어진 큐는 삽입과 삭제시 시간복잡도에서 상수시간을 가진다.
   // 검색와 인덱스 접근시에는 선형시간
   class Node {
     #next = null;
@@ -35,25 +37,26 @@ const stackInLinkedList = () => {
     }
   }
 
-  class Stack {
+  // 선입선출
+  class queue {
     #first = null;
     #last = null;
     #size = 0;
 
     // first에 새로운 데이터를 추가한다.
-    push(val) {
+    enqueue(val) {
       const newNode = new Node(val);
       if (!this.#first) {
         this.#first = newNode;
         this.#last = newNode;
       } else {
-        newNode.next = this.#first;
-        this.#first = newNode;
+        this.#last.next = newNode;
+        this.#last = newNode;
       }
       this.#size++;
     }
 
-    pop() {
+    dnqueue() {
       if (this.#size === 0) return null;
       const popNode = this.#first;
       if (this.#size === 1) this.#last = null;
@@ -70,16 +73,13 @@ const stackInLinkedList = () => {
       });
     }
   }
-  const stack = new Stack();
-  stack.push(1);
-  stack.push(2);
-  console.log(stack.pop());
-  console.log(stack.pop());
-  // stack.pop();
-  // stack.push(3);
-  stack.toString();
-  // const f = stack.
-
-  // console.log(stack.toString());
+  const q = new queue();
+  q.enqueue(1);
+  q.enqueue(2);
+  q.enqueue(3);
+  console.log(q.dnqueue());
+  console.log(q.dnqueue());
+  console.log(q.dnqueue());
+  console.log(q.toString());
 };
-stackInLinkedList();
+queueInLinkedList();
