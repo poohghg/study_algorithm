d42623a620b977112a7b1a7221c634305634c740
khg <poohghg@naver.com>
2024-11-16 17:19:16 +0900

    feat: 기본 알고리즘 풀이

--- src/2024/4.완전탐색_블루투포스.ts
+++ src/2024/4.완전탐색_블루투포스.ts
@@ -24,21 +24,23 @@ const solution1 = (nums: number[]) => {
     // }, 0);
   };
 
-  let result = Number.MIN_SAFE_INTEGER;
-  let maxSumOfDigits = Number.MIN_SAFE_INTEGER;
+  const getMaxSumOfDigits = () => {
+    let result = Number.MIN_SAFE_INTEGER;
+    let maxSumOfDigits = Number.MIN_SAFE_INTEGER;
 
-  for (const num of nums) {
-    const sum = sumOfDigits(num);
+    for (const num of nums) {
+      const sum = sumOfDigits(num);
 
-    if (sum > maxSumOfDigits) {
-      result = num;
-      maxSumOfDigits = sum;
-    } else if (sum === maxSumOfDigits) {
-      result = Math.max(result, num);
+      if (sum > maxSumOfDigits) {
+        result = num;
+        maxSumOfDigits = sum;
+      } else if (sum === maxSumOfDigits) {
+        result = Math.max(result, num);
+      }
     }
-  }
+  };
 
-  return result;
+  return getMaxSumOfDigits();
 };
 
 // console.log(solution1([128, 460, 603, 40, 521, 137, 123]));
@@ -79,8 +81,69 @@ const solution2 = (nums: number[]) => {
     if (isPrime(reverseNum)) {
       result.push(reverseNum);
     }
+
     return result;
   }, []);
 };
 
-console.log(solution2([32, 55, 62, 20, 250, 370, 200, 30, 100]));
+// console.log(solution2([32, 55, 62, 20, 250, 370, 200, 30, 100]));
+
+/**
+ * 현수네 반 선생님은 반 학생들의 수학점수를 향상시키기 위해 멘토링 시스템을 만들려고 합니 다. 멘토링은 멘토(도와주는 학생)와 멘티(도움을 받는 학생)가 한 짝이 되어 멘토가 멘티의 수학공부를 도와주는 것입니다.
+ * 선생님은 M번의 수학테스트 등수를 가지고 멘토와 멘티를 정합니다.
+ * 만약 A학생이 멘토이고, B학생이 멘티가 되는 짝이 되었다면, A학생은 M번의 수학테스트에서 모두 B학생보다 등수가 앞서야 합니다.
+ * M번의 수학성적이 주어지면 멘토와 멘티가 되는 짝을 만들 수 있는 경우가 총 몇 가지 인지 출력하는 프로그램을 작성하세요.
+ * 학생번호가 제일 앞에서부터 1등, 2등, ...N등 순으로 표현된다.
+ */
+
+const solution3 = (scores: number[][]) => {
+  // 뽑히는 사람은 모든 시험에서 앞서야 한다.
+  // 스코어의 인덱스는 등수순이다.
+  // 블루투스 버스 가능한 모든 경우의 수를 생각해서 만든다.
+  // 가능한 경우의 수를 먼저 확인 4 * 3
+  // 1 2 3 4
+  // 1 2 3 4
+
+  // const sortedScoresOfIndex =
+
+  const canMentor = (mentor: number, mentee: number) => {
+    for (const score of scores) {
+      const mentorRank = score.indexOf(mentor);
+      const memteeRank = score.indexOf(mentee);
+
+      if (mentorRank > memteeRank) {
+        return false;
+      }
+    }
+    return true;
+  };
+
+  const studnetSzie = scores[0].length;
+  const initCadinate = Array.from({ length: studnetSzie }, () => false);
+  let result = 0;
+
+  for (let i = 1; i <= studnetSzie; i++) {
+    const cadinate = [...initCadinate];
+
+    for (let j = 1; j <= studnetSzie; j++) {
+      if (i === j) continue;
+
+      if (canMentor(i, j)) {
+        cadinate[j - 1] = true;
+      } else if (cadinate[j - 1]) {
+        cadinate[j - 1] = false;
+      }
+    }
+    result += cadinate.filter(Boolean).length;
+  }
+
+  return result;
+};
+
+console.log(
+  solution3([
+    [3, 4, 1, 2],
+    [4, 3, 2, 1],
+    [3, 1, 4, 2],
+  ]),
+);
