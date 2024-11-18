5ec50b48cf273ac1387b384c050476d28e251ad1
khg <poohghg@naver.com>
2024-11-18 15:00:36 +0900

    feat: 기본 알고리즘 풀이

--- src/2024/4.완전탐색_블루투포스.ts
+++ src/2024/4.완전탐색_블루투포스.ts
@@ -195,36 +195,30 @@ const solution4 = (budget: number, priceInfo: [number, number][]) => {
 //   ]),
 // );
 
-function nestedLoops(n: number, depth = 0, result: number[] = []) {
-  if (depth === n) {
-    console.log(result); // 각 루프 결과 출력
-    return;
-  }
-
-  for (let i = 0; i < 3; i++) {
-    // 각 루프에서 3번 반복
-    result[depth] = i;
-    nestedLoops(n, depth + 1, result);
-  }
-}
-
+/**
+ * k번째 큰 수
+ * 현수는 1부터 100사이의 자연수가 적힌 N장의 카드를 가지고 있습니다. 같은 숫자의 카드가 여러장 있을 수 있습니다. 현수는 이 중 3장을 뽑아 각 카드에 적힌 수를 합한 값을 기록하려 고 합니다. 3장을 뽑을 수 있는 모든 경우를 기록합니다. 기록한 값 중 K번째로 큰 수를 출력 하는 프로그램을 작성하세요.
+ * 만약 큰 수부터 만들어진 수가 25 25 23 23 22 20 19......이고 K값이 3이라면 K번째 큰 값 은 22입니다.
+ */
 const solution5 = (k: number, nums: number[]) => {
   const n = 3;
+  const result: number[] = [];
 
-  function nestedLoops(index: number, depth: number, sum: number) {
-    // console.log(sum);
-
-    if (3 <= depth) {
-      return sum;
+  const nestedLoops = (start: number, depth: number, sum: number) => {
+    if (depth === n) {
+      result.push(sum);
+      return;
     }
 
-    for (let i = index + depth; i < nums.length; i++) {
-      const num = nestedLoops(i, depth + 1, sum + nums[i]);
-      console.log(num);
+    for (let i = start; i < nums.length; i++) {
+      nestedLoops(i + 1, depth + 1, sum + nums[i]);
     }
-  }
+  };
 
   nestedLoops(0, 0, 0);
+  const sortedResult = result.slice().sort((a, b) => b - a);
+
+  return sortedResult[k - 1];
 };
 
-console.log(solution5(3, [1, 2]));
+console.log(solution5(3, [13, 15, 34, 23, 45, 65, 33, 11, 26, 42]));
