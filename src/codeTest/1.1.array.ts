c669a0c484322928134fd5b81b38297173592cd7
khg <poohghg@naver.com>
2026-04-15 17:14:11 +0900

    feat: 기본 알고리즘 풀이

--- src/codeTest/1.1.array.ts
+++ src/codeTest/1.1.array.ts
@@ -1,5 +1,39 @@
 export default {};
 
+function minNumberOfSeconds(
+  mountainHeight: number,
+  workerTimes: number[],
+): number {
+  const maxT: number = Math.max(...workerTimes);
+  const n: number = workerTimes.length;
+  const v: number = Math.ceil(mountainHeight / n);
+
+  let start: number = 0;
+  let end: number = (maxT * v * (v + 1)) / 2;
+  let res: number = end;
+
+  while (start <= end) {
+    let mid: number = Math.floor(start + (end - start) / 2);
+    let totalHeight: number = 0;
+
+    for (const t of workerTimes) {
+      const x: number = Math.floor((-1 + Math.sqrt(1 + (8 * mid) / t)) / 2);
+      totalHeight += x;
+      if (totalHeight >= mountainHeight) break;
+    }
+
+    if (totalHeight >= mountainHeight) {
+      res = mid;
+      end = mid - 1;
+    } else {
+      start = mid + 1;
+    }
+  }
+  return res;
+}
+
+console.log(minNumberOfSeconds(10, [3, 2, 2, 4]));
+
 function countPalindromicSubsequence(s: string): number {
   const chars = new Set(s);
 
@@ -37,6 +71,8 @@ function getMinDistance(nums: number[], target: number, start: number): number {
 // console.log(getMinDistance([1, 1, 1, 1, 1], 1, 1));
 
 function minSubarray(nums: number[], p: number): number {
+  const total = nums.reduce((a, b) => a + b);
+
   const mods = Array(p).fill(-1);
   mods[0] = 0;
 
@@ -51,7 +87,7 @@ function minSubarray(nums: number[], p: number): number {
   return 1;
 }
 
-console.log(minSubarray([6, 4, 5, 1, 5], 5));
+// console.log(minSubarray([6, 4, 5, 1, 5], 5));
 
 /**
  * https://leetcode.com/problems/subarray-sums-divisible-by-k/
